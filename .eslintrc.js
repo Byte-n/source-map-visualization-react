fb4f1ebaab0e4273c4df358454e14cf8f0756d26
Byte-n <Byte-n@users.noreply.github.com>
2025-09-07 08:34:20 +0000

    deploy: 1fe85d5d6f4fa41b20969a1e204a0c1d183e4227

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: require.resolve('@umijs/lint/dist/config/eslint'),
-  rules: {
-    '@typescript-eslint/no-use-before-define': 'off',
-    '@typescript-eslint/no-empty-interface': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
-  },
-};
